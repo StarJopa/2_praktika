f3686a3c219b6900bbb5d5b5332851a5eff0c682
New <New@KOMPUTER>
2024-11-18 14:17:12 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfTasks
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonTask1_Click(object sender, RoutedEventArgs e) { new Task1Window().ShowDialog(); }
+        private void ButtonTask2_Click(object sender, RoutedEventArgs e) { new Task2Window().ShowDialog(); }
+        private void ButtonTask3_Click(object sender, RoutedEventArgs e) { new Task3Window().ShowDialog(); }
+        private void ButtonTask4_Click(object sender, RoutedEventArgs e) { new Task4Window().ShowDialog(); }
+    }
+}
---
+++ Task4Window.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WpfTasks
+{
+    public partial class Task4Window : Window
+    {
+        private string secretPassword = "password123"; 
+        private string secretMessage = "Это секретное сообщение!";
+
+        public Task4Window()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCheck_Click(object sender, RoutedEventArgs e)
+        {
+            string enteredPassword = passwordBox.Password;
+            if (enteredPassword == secretPassword)
+            {
+                textBlockMessage.Text = secretMessage;
+                passwordBox.Clear(); 
+            }
+            else
+            {
+                MessageBox.Show("Неверный пароль. Попробуйте еще раз.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                passwordBox.Clear(); 
+            }
+        }
+    }
+}
